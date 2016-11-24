17208ad552f4493a9e5d8d63237a160a384fb5ed
12DReflections <julianadamwise@gmail.com>
2016-11-24 12:17:36 +1100

    httpresponse used in musit/urls.py

--- music/urls.py
+++ music/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
 		#/music/
 	url('^$', views.index, name='index'),
 		
-		#/music/712  (?P<foo>) creates an object foo, 
+		# Database Entry
+		# /music/712  (?P<foo>) creates an object foo, 
 		#              pull out the number before the slash,
 		# 			   treat as album_id
 	url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
--- music/views.py
+++ music/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Album
 
 # Create your views here.
 def index(request):
-	return HttpResponse("<h1>This is a music website</h1>")
+	# Query all database objects,
+	all_albums = Album.objects.all()
+	# Assign the template for the view
+	template = loader.get_template('music/index.html')
+	context = {
+			'all_albums': all_albums,
+	}
+
+	return HttpResponse(template.render(context,request))
 
 def detail(request, album_id):
 	return HttpResponse("<h2>Details for view " + str(album_id) +" </h2>" )
