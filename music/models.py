91a17eea2814efe074d35ce2c5883c8c196da0fb
12DReflections <julianadamwise@gmail.com>
2016-11-24 13:23:29 +1100

    details template

--- music/models.py
+++ music/models.py
@@ -14,3 +14,6 @@ class Song(models.Model):
 	album = models.ForeignKey(Album, on_delete=models.CASCADE) # When deleting an album delete the songs
 	file_type = models.CharField(max_length=100)
 	song_title = models.CharField(max_length=250)
+
+	def __str__(self):
+		return self.song_title
--- music/views.py
+++ music/views.py
@@ -1,18 +1,18 @@
+from django.http import Http404
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Album
 
 # Create your views here.
 def index(request):
 	# Query all database objects,
 	all_albums = Album.objects.all()
-	# Assign the template for the view
-	template = loader.get_template('music/index.html')
-	context = {
-			'all_albums': all_albums,
-	}
+				# request  	template			context
+	return render(request, 'music/index.html', {'all_albums': all_albums })
 
-	return HttpResponse(template.render(context,request))
-
+# Get a single album
 def detail(request, album_id):
-	return HttpResponse("<h2>Details for view " + str(album_id) +" </h2>" )
+	try:
+		album = Album.objects.get(pk=album_id)
+	except Album.DoesNotExist:
+		raise Http404("Album could not be found")
+	return render(request, 'music/detail.html', {'album': album })
